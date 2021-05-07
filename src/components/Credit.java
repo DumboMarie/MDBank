4e2ac687d754dd819e2916ab1e3c4323ca02f42e
Dumbo Marie <marie_dumbo@live.fr>
2021-05-07 09:01:50 +0200

    Creation Transfert, Credit and Debit class
    HN institut, TP N°1 v2 (Java 8), Exercice 1.3.3 Creation of the
    Transfert, Credit, Debit classes

---
+++ src/components/Credit.java
@@ -0,0 +1,6 @@
+// 1.3.3 Creation of the Transfert, Credit, Debit classes
+package components;
+
+public class Credit extends Flow {
+
+}
---
+++ src/components/Transfert.java
@@ -0,0 +1,7 @@
+// 1.3.3 Creation of the Transfert, Credit, Debit classes
+package components;
+
+public class Transfert extends Flow {
+	public String transfertAccountID;
+
+}
